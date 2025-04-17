459b2e28edf300b3ae61dc71a808c765765c1d99
Ananya Jajoo <jajooananya@gmail.com>
2025-04-16 22:04:53 -0400

    auth

--- BoilerMake/AppDelegate.swift
+++ BoilerMake/AppDelegate.swift
@@ -7,21 +7,24 @@
 
 import UIKit
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        FirebaseApp.configure()
+        
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let mainView = MainAppView()
 
         // Use a UIHostingController as window root view controller.
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UIHostingController(rootView: contentView)
+        window.rootViewController = UIHostingController(rootView: mainView)
         self.window = window
         window.makeKeyAndVisible()
         return true
